fecbdfc99285b08fc5d8228c7e58f381beb28818
manikantakoppula <manikanta.k@servicepack.ai>
2024-09-12 18:45:34 +0530

    functionality is done

--- main.py
+++ main.py
@@ -1,16 +1,73 @@
 import json
 import os
 import argparse
+from datetime import datetime
+from messages import *
+global base_dict,status_list ,filename,time_format
 
-global filename 
 filename ="task_data.json"
+base_dict = {"description":"","status":"todo","createdAt":"","updatedAt":""}
+time_format = "%Y-%m-%dT%H:%M:%S"
+status_list = [
+    "done",
+    "todo",
+    "in-progress"
+    ]
+
+###################### parsering ###################################
 parser = argparse.ArgumentParser(description='Task Tracker')
 parser.add_argument('data', type=str,help='[action] [id or description] [sub-action]',nargs='*')
 args = parser.parse_args()
 
 def add(id,description,pev_data:dict):
-    pev_data[id] = description
+    time_now = datetime.now()
+    base_dict["description"]= description
+    base_dict['createdAt'] = time_now.strftime(time_format)
+    base_dict['updatedAt'] = time_now.strftime(time_format)
+    pev_data[id] = base_dict
     return f"Task added successfully (ID: {id})"
+
+
+def searchList(args,pev_data:dict):
+    try:
+        condition=args.data[1]
+        if condition in status_list:
+            print("| ID |  STATUS   |    CREATEDAT    |    UPDATEDAT    |    DESCRIPTION    |")                
+            for key,entry in pev_data.items():
+                if entry['status'] == condition:
+                    print(f"|{key}|{entry['status']}|{entry['createdAt']}|{entry['updatedAt']}|{entry['description']}|")
+        else:
+            return f"please select only in this status: \n{status_list}"
+    except IndexError:
+        print("| ID |  STATUS   |    CREATEDAT    |    UPDATEDAT    |    DESCRIPTION    |")                
+        for key,entry in pev_data.items():
+            print(f"|{key}|{entry['status']}|{entry['createdAt']}|{entry['updatedAt']}|{entry['description']}|")
+    except Exception as e:
+        return e
+def delete(id,pev_data):
+    del pev_data[id]
+    return f"Task successfully deleted (ID: {id})"
+
+def update(id,description,pev_data:dict):
+    time_now = datetime.now()
+    temp_dict = pev_data.get(id)
+    if not temp_dict:
+        return vaild_id(id)
+    
+    temp_dict["description"]= description
+    temp_dict['updatedAt'] = time_now.strftime(time_format)
+    pev_data[id] = temp_dict
+    return f"Task updated successfully (ID: {id})"
+
+def status_update(id,status,pev_data:dict):
+    time_now = datetime.now()
+    temp_dict = pev_data.get(id)
+    if not temp_dict:
+        return vaild_id(id)
+    temp_dict["status"]= status
+    temp_dict['updatedAt'] = time_now.strftime(time_format)
+    pev_data[id] = temp_dict
+    return f"Task status successfully (ID: {id})"
 
 def main(args,pev_data:dict):
     try:
@@ -18,29 +75,47 @@ def main(args,pev_data:dict):
         action = args.data[0].lower()
 
         if action == 'add':
-            _id = int(max(pev_data.keys()))+1
+            indexs = pev_data.keys()
+            if indexs:
+                _id = int(max(indexs))+1
+            else: 
+                _id = 1
+
             description = args.data[1]
-            message = add(_id,description,pev_data)
-        elif action in ['update','delete']:
+            message=add(_id,description,pev_data)
+
+        elif action in 'delete':
+            _id = args.data[1]
+
+            message=delete(_id,pev_data)
+
+        elif action in 'update':
             _id = args.data[1]
             description = args.data[2]
-        
+            message=update(_id,description,pev_data)
+
         elif action.startswith('mark'):
             _id = args.data[1]
-            action ='mark'
-            description = action[4:]
+            status_type = action[5:].lower()
+            if status_type in status_list:
+                message=status_update(_id,status_type,pev_data)
+            else:
+                message = f"please select only in this status: \n{status_list}"
         
         elif action == 'list':
-            description = args.data[1]
-        writeData(pev_data,filename)
-        print(message) 
-    
-    except IndexError:
-        print("For help please use -h")
+            message = searchList(args,pev_data)
+        
+        else:
+            message="For help please use -h"
+        writeData(pev_data)
+        print(message)
+
+    except Exception as e:
+        print(e)
         return None
  
 
-def writeData(entry,filename):
+def writeData(entry):
     try:
         with open(filename, 'w') as file:
             json.dump(entry,fp=file)
@@ -49,17 +124,20 @@ def writeData(entry,filename):
         print(e)
         return False
 
-def readData(filename):
+def readData():
     task_data=dict()
     try:
         with open(filename, 'r') as file:
             task_data=json.load(fp=file)
         return task_data
     except Exception as e:
-        print(e)
+        print("No data")
         return task_data
 
 
 
-pev_data = readData(filename)
-user_input = main(args,pev_data)
+pev_data = readData()
+output = main(args,pev_data)
+
+
+
---
+++ messages.py
@@ -0,0 +1,2 @@
+def vaild_id(_id):
+    return f"Please enter a vaild Task ID.{_id} is not found"
